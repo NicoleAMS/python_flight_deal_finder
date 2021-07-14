8fdb38e4b55ec811e4d3d694e254e09bbeb9849a
NicoleAMS <nicole.sentis@gmail.com>
2021-07-14 17:49:57 +0200

    registers new users and adds their data to Google Sheets

--- data_manager.py
+++ data_manager.py
@@ -11,17 +11,36 @@ class DataManager:
         }
         self.data = []
 
-    def get_sheet_data(self):
-        response = requests.get(url=self.endpoint, headers=self.header)
+    def get_flight_data(self):
+        response = requests.get(url=f"{self.endpoint}/prices", headers=self.header)
         result = response.json()
         self.data = result['prices']
         return result['prices']
 
-    def update_sheet_data(self, row_id, payload):
-        url = f"{self.endpoint}/{row_id}"
+    def update_flight_data(self, row_id, payload):
+        url = f"{self.endpoint}/prices/{row_id}"
         body = {
             "price": {key: payload[key] for key in payload}
         }
         response = requests.put(url=url, json=body, headers=self.header)
         result = response.json()
         print(result)
+
+    def add_user(self, user):
+        url = f"{self.endpoint}/users"
+        body = {
+            "user": {
+                "firstName": user.first_name,
+                "lastName": user.last_name,
+                "email": user.email
+            }
+        }
+        response = requests.post(url=url, json=body, headers=self.header)
+        result = response.json()
+        print(result)
+
+    def get_users(self):
+        response = requests.get(url=f"{self.endpoint}/users", headers=self.header)
+        result = response.json()
+        self.data = result['users']
+        return result['users']
